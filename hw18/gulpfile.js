0c0248b80683977edc2891f7a58576977cc1721b
UrupaDev <taniaivanko@ukr.net>
2022-08-29 17:41:32 +0100

    hw18

--- hw18/gulpfile.js
+++ hw18/gulpfile.js
@@ -1,10 +1,16 @@
 let gulp = require('gulp'),
+    autoprefixer = require("gulp-autoprefixer"),
     sass = require('gulp-sass')(require('sass'));
 
 
 function buildStyles() {
     return gulp.src('assets/scss/**/*.scss', "!" + "assets/scss/**/_*.scss")
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+                overrideBrowserList: ["last 5 versions"],
+                cascade: true
+            })
+        )
         .pipe(gulp.dest('src/css'));
 }
 
@@ -18,4 +24,3 @@ let watch = gulp.parallel(build, watchFiles);
 exports.buildStyles = buildStyles;
 exports.default = watch;
 exports.build = build;
-
