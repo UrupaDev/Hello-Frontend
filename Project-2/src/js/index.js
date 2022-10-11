31875f3d63b926e53b786c7d40ee2172bc4ebf3f
UrupaDev <taniaivanko@ukr.net>
2022-10-11 12:38:02 +0100

    Project-2

--- Project-2/src/js/index.js
+++ Project-2/src/js/index.js
@@ -9,6 +9,11 @@ Fancybox.bind('[data-fancybox="gallery"]', {
 
 //swiper for news
 const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 4000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+    },
   direction: 'horizontal',
   loop: true,
   pagination: {
@@ -360,3 +365,67 @@ function initMap() {
   });
 }
 window.initMap = initMap;
+
+// wow animations
+wow = new WOW(
+  {
+  boxClass:     'wow',    
+  animateClass: 'animate__animated',
+  offset:       30,
+  mobile:       false,
+  live:         true 
+}
+)
+wow.init();
+
+// form validation
+let form = document.querySelector('form');
+let username = document.querySelector('#name');
+let email = document.querySelector('#email');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailPattern.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+
+  if(usernameValue === '') {
+      setError(username, 'Username is required');
+  } else {
+      setSuccess(username);
+  }
+
+  if(emailValue === '') {
+    setError(email, 'Email is required');
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, 'Provide a valid email address');
+  } else {
+    setSuccess(email);
+  }
+};
